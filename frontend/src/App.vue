b4b6fe37205e92a69170a7bfd7d13c24d4c994fe
James Evans <jamesburtonevans@gmail.com>
2025-09-09 09:24:13 -0400

    Database integration and foundational backend setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import axios from 'axios'
+  import {ref, onMounted} from 'vue'
+  const tasks = ref([])
+
+  async function fetchTasks() {
+    const response = await axios.get('http://localhost:5000/tasks')
+    tasks.value = response.data
+  }
+  
+  onMounted(() => {
+    fetchTasks()
+  })
+</script>
+
+<template>
+  <h1>Here are your tasks!</h1>
+  <p>
+    <li v-for="name in tasks">
+      {{ task.name }}
+    </li>
+  </p>
+</template>
+
+<style scoped></style>
